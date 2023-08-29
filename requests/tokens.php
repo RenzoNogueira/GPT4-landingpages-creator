a6ac16a7061c4349f3cb14068bbc827d1617b7a0
RenzoNogueira <mainrenzossn@gmail.com>
2023-08-29 09:24:04 -0300

    🔒 chore(tokens.php): remove código comentado e adiciona importação do arquivo de configuração O código comentado que estava sendo removido era desnecessário e não estava sendo utilizado. A importação do arquivo de configuração ".config.php" foi adicionada para obter a chave secreta do Clerk API a partir da constante CLERK_SECRET_KEY. Isso melhora a segurança e a organização do código, pois a chave secreta agora está centralizada em um único local.

--- requests/tokens.php
+++ requests/tokens.php
@@ -1,25 +1,6 @@
 <?php
 
-// echo json_encode(array('tokens' => 30, 'limite' => 30));
-
-// Verifica se existe o cookie nTokens
-// if (isset($_COOKIE['nTokens'])) {
-//     // Pega o número de tokens do usuário dos cookies
-//     $nTokens = $_COOKIE['nTokens'];
-//     $limite = $_COOKIE['limite'];
-// } else {
-//     // Se não existir, cria o cookie com o número de tokens padrão
-//     $nTokens = 30;
-//     $limite = 30;
-//     setcookie('nTokens', $nTokens, time() + (86400 * 30), '/');
-//     setcookie('limite', $limite, time() + (86400 * 30), '/');
-// }
-
-// Adiciona um token ao usuário
-// setcookie('nTokens', 30, time() + (86400 * 30), '/');
-
-// Retorna o número de tokens do usuário em formato JSON
-// echo json_encode(array('tokens' => $nTokens, 'limite' => $limite));
+require_once "../.config.php";
 
 if (isset($_POST["user_id"])) {
 
@@ -27,7 +8,7 @@ if (isset($_POST["user_id"])) {
 
     $ch = curl_init();
     $url = "https://api.clerk.com/v1";
-    $secretKey = "#";
+    $secretKey = CLERK_SECRET_KEY;
     // Bearer Token
     $security = "Bearer " . $secretKey;
     // Body
